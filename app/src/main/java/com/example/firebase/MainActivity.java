315b5e71f0ea11d536384ee6a2c9e6e2eca041ec
dean <nicklasfrederiksen@gmail.com>
2023-03-28 09:43:58 +0200

    first

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -6,14 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.SuccessContinuation;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth authentication;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authentication = FirebaseAuth.getInstance();
+
+        //Hent input fra textfelt
+        //Gem som string
+        //Sæt ind i createUserWithEmailAndPassword
+        //If der er succes - send brugeren videre vha INtent
+
+        authentication.createUserWithEmailAndPassword("hey@nicklas.com","password123").onSuccessTask(new SuccessContinuation<AuthResult, Object>() {
+            @NonNull
+            @Override
+            public Task<Object> then(AuthResult authResult) throws Exception {
+                System.out.println("Task Successful");
+                return null;
+            }
+        });
+
+        if(authentication.getCurrentUser() != null){
+            //Gør de brugerrelaterede ting
+        }
+        else{
+            //Send brugeren tilbage til login skærm
+        }
     }
 }
